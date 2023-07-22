acbfff287d3ee7150ae1f0141016c44c9e2754bd
Thomas Ruyter <31480147+TomRuyter@users.noreply.github.com>
2023-07-22 11:50:40 +0100

    Working LeetCode 67

--- LeetCode/LeetCode/67-AddBinary.cs
+++ LeetCode/LeetCode/67-AddBinary.cs
@@ -1,3 +1,5 @@
+using System.Text;
+
 namespace LeetCode
 {
     [TestClass]
@@ -5,10 +7,11 @@ namespace LeetCode
     {
         [DataTestMethod]
         [DataRow("11", "1", "100")]
+        [DataRow("1111", "1111", "11110")]
         [DataRow("1010", "1011", "10101")]
-        public void AddBinary(string a, string b, string output)
+        public void AddBinary(string a, string b, string expected)
         {
-            Assert.AreEqual(new Solution().AddBinary(a, b), output);
+            Assert.AreEqual(expected, new Solution().AddBinary(a, b));
         }
     }
 
@@ -19,7 +22,45 @@ namespace LeetCode
     {
         public string AddBinary(string a, string b)
         {
-            return string.Empty;
+            // Variables
+            var carry = 0;
+            var result = new StringBuilder();
+
+            // Normalise strings. Easier for loops
+            if (a.Length > b.Length) { b = b.PadLeft(a.Length, '0'); }
+            if (b.Length > a.Length) { a = a.PadLeft(b.Length, '0'); }
+
+            // loop through characters
+            for (int i = a.Length - 1; i >= 0; i--)
+            {
+                var value = int.Parse(a.Substring(i, 1)) + int.Parse(b.Substring(i, 1)) + carry;
+
+                if (value > 1)
+                {
+                    if (value == 2)
+                    {
+                        carry = 1;
+                        result.Append('0');
+                    }
+
+                    if (value == 3)
+                    {
+                        carry = 1;
+                        result.Append('1');
+                    }
+                }
+                else
+                {
+                    carry = 0;
+                    result.Append(value);
+                }
+            }
+
+            // Still have left overs?
+            if (carry > 0) { result.Append('1'); }
+
+            // pass back what we have, but reverse it.
+            return new string(result.ToString().Reverse().ToArray());
         }
     }
 }
