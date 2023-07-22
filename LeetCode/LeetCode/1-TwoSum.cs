e0ef223396397326e3797434a43ca20af86cca49
Thomas Ruyter <31480147+TomRuyter@users.noreply.github.com>
2023-07-22 12:43:53 +0100

    Added TwoSum

---
+++ LeetCode/LeetCode/1-TwoSum.cs
@@ -0,0 +1,58 @@
+﻿using System.Text;
+
+namespace LeetCode
+{
+    [TestClass]
+    public class TwoSum_1
+    {
+        [DataTestMethod]
+        [DataRow(new int[] { 3, 4 }, 7, new int[] { 0, 1 })]
+        [DataRow(new int[] { 3, 2, 4 }, 6, new int[] { 1, 2 })]
+        [DataRow(new int[] { 2, 7, 11, 15 }, 9, new int[] { 0, 1 })]
+        public void TwoSum(int[] nums, int target, int[] expected)
+        {
+            var result = new Solution().TwoSum(nums, target);
+
+            for (int i = 0; i < expected.Length; i++)
+            {
+                Assert.AreEqual(expected[i], result[i]);
+            }
+        }
+    }
+
+    public partial class Solution
+    {
+        public int[] TwoSum(int[] nums, int target)
+        {
+            // Probably need this for figuring out the index of the ones that match.
+            // i can't figure another way of getting lock on the index we need.
+            var numbers = new Dictionary<int, int>();
+
+            // Go through each number.
+            for (int i = 0; i < nums.Length; i++)
+            {
+                // Add the numbers into the list.
+                numbers.Add(i, nums[i]);
+
+                if (numbers.Count > 1)
+                {
+                    // See if we can find a match
+                    foreach (var item in numbers)
+                    {
+                        if (item.Key != i)
+                        {
+                            if (item.Value + nums[i] == target)
+                            {
+                                return new int[] { item.Key, i };
+                            }
+
+                        }
+                    }
+                }
+
+            }
+
+            return new int[] { 0, 0 };
+        }
+    }
+}
