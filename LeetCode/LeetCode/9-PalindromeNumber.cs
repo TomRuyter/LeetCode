e1fefd096464d2a1901b214a87df207094cb8c70
TomRuyter <31480147+TomRuyter@users.noreply.github.com>
2023-07-26 21:47:15 +0100

    Added LeeetCode 9 (PalindromeNumber)

---
+++ LeetCode/LeetCode/9-PalindromeNumber.cs
@@ -0,0 +1,29 @@
+﻿namespace LeetCode
+{
+    [TestClass]
+    public class PalindromeNumber_9
+    {
+        [DataTestMethod]
+        [DataRow(121, true)]
+        [DataRow(-121, false)]
+        [DataRow(10, false)]
+        public void PalindromeNumber(int number, bool expected)
+        {
+            Assert.AreEqual(expected, Solution.IsPalindrome(number));
+        }
+    }
+
+    public partial class Solution
+    {
+        public static bool IsPalindrome(int x)
+        {
+            // See if this can't be reversed
+            if (x < 0) return false;
+
+            // Use strings.
+            var array = x.ToString().ToCharArray();
+            Array.Reverse(array);
+            return x.ToString() == new string(array);
+        }
+    }
+}
