ce0d572e4ced099a817de67a84e4e87c2d77cf8f
TomRuyter <31480147+TomRuyter@users.noreply.github.com>
2023-07-27 21:53:13 +0100

    Added New LeetCode "Medium" Number 75

---
+++ LeetCode/LeetCode/75-SortColors.cs
@@ -0,0 +1,65 @@
+﻿namespace LeetCode
+{
+    [TestClass]
+    public class SortColors_75
+    {
+        [DataTestMethod]
+        [DataRow(new int[] { 2, 0, 2, 1, 1, 0 }, new int[] { 0, 0, 1, 1, 2, 2 })]
+        [DataRow(new int[] { 2, 0, 1 }, new int[] { 0, 1, 2 })]
+        [DataRow(new int[] { 0, 0, 1 }, new int[] { 0, 0, 1 })]
+        [DataRow(new int[] { 2, 2, 0 }, new int[] { 0, 2, 2 })]
+        public void SortColors(int[] nums, int[] expected)
+        {
+            Solution.SortColors(nums);
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                Assert.AreEqual(expected[i], nums[i]);
+            }
+        }
+    }
+
+    public partial class Solution
+    {
+        public static void SortColors(int[] nums)
+        {
+            var colourCounts = new Dictionary<int, int>
+            {
+                { 0, 0 },
+                { 1, 0 },
+                { 2, 0 }
+            };
+
+            // Store the amounts of each number.
+            for (int i = 0; i < nums.GetLength(0); i++)
+            {
+                colourCounts[nums[i]] += 1;
+            }
+
+            // Write them back out.
+            for (int i = 0; i < nums.GetLength(0); i++)
+            {
+                if (colourCounts[0] > 0)
+                {
+                    nums[i] = 0;
+                    colourCounts[0] -= 1;
+                    continue;
+                }
+
+                if (colourCounts[1] > 0)
+                {
+                    nums[i] = 1;
+                    colourCounts[1] -= 1;
+                    continue;
+                }
+
+                if (colourCounts[2] > 0)
+                {
+                    nums[i] = 2;
+                    colourCounts[2] -= 1;
+                    continue;
+                }
+            }
+        }
+    }
+}
